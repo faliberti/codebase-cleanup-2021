a7adad2a0c6b50091a448353a7ea2912d513e3d8
faliberti <69914057+faliberti@users.noreply.github.com>
2021-04-06 23:21:39 -0400

    Simplify and Test Shop.py (#3)
    Simplified the code for the shopping project as well as automatically testing it.

--- app/shopping.py
+++ app/shopping.py
@@ -1,6 +1,27 @@
 import os
-from datetime import datetime
+import datetime
 from pandas import read_csv
+
+# defining functions
+
+def format_usd(my_price):
+    """
+    Formats a number as USD with a dollar sign and two decimals
+    Paramaters: my_price is a number that is formated
+    """
+    return f"${my_price:,.2f}"
+
+def lookup_product(product_id, all_products):
+    """
+    Paramaters :
+        product_id (str)
+        all_products (list of dict) each dict with an "id", "name", "department", "aisle", and "price" attribute
+    """
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products):
+        return matching_products[0]
+    else:
+        return None
 
 # READ INVENTORY OF PRODUCTS
 
@@ -22,40 +43,38 @@ while True:
         else:
             print("OOPS, Couldn't find that product. Please try again.")
 
-checkout_at = datetime.now()
+date = datetime.date.today()
+time = datetime.datetime.now()
 
 subtotal = sum([float(p["price"]) for p in selected_products])
 
 # PRINT RECEIPT
+checkout_datetime_format = str(date) + " " + str(time.strftime("%I:%M:%S %p"))
+tax_rate = 0.0875
+tax_amount = tax_rate * subtotal
+total_amount = subtotal + tax_amount
 
-print("---------")
-print("CHECKOUT AT: " + str(checkout_at.strftime("%Y-%M-%d %H:%m:%S")))
-print("---------")
+receipt = ""
 for p in selected_products:
-    print("SELECTED PRODUCT: " + p["name"] + "   " + '${:.2f}'.format(p["price"]))
+    receipt += "SELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]) + "\n"
+receipt += "---------\n"
+receipt += f"SUBTOTAL: {format_usd(subtotal)}\n"
+receipt += f"TAX: {format_usd(tax_amount)}\n"
+receipt += f"TOTAL: {format_usd((total_amount))}\n"
+receipt += "---------\n"
+receipt += "THANK YOU! PLEASE COME AGAIN SOON!\n"
+receipt += "---------\n"
 
 print("---------")
-print(f"SUBTOTAL: {subtotal:,.2f}")
-print(f"TAX: {(subtotal * 0.0875):.2f}")
-print(f"TOTAL: {((subtotal * 0.0875) + subtotal):.2f}")
+print("CHECKOUT AT: " + checkout_datetime_format)
 print("---------")
-print("THANK YOU! PLEASE COME AGAIN SOON!")
-print("---------")
+print(receipt)
 
 # WRITE RECEIPT TO FILE
 
-receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
+receipt_id = checkout_datetime_format
 receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
 
 with open(receipt_filepath, "w") as receipt_file:
-    receipt_file.write("------------------------------------------")
-    for p in selected_products:
-        receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
-
-    receipt_file.write("\n---------")
-    receipt_file.write(f"\nSUBTOTAL: {subtotal}")
-    receipt_file.write(f"\nTAX: {subtotal * 0.875}")
-    receipt_file.write(f"\nTOTAL: {((subtotal * 0.875) + subtotal)}")
-    receipt_file.write("\n---------")
-    receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
-    receipt_file.write("\n---------")
+        receipt_file.write("------------------------------------------\n")
+        receipt_file.write(receipt)
