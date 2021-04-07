ceda0b2261caa6a5a2dcdd8123412f6cdebc4c42
faliberti <69914057+faliberti@users.noreply.github.com>
2021-04-06 22:22:04 -0400

    updated the test for determine_winner (#2)

--- test/game_test.py
+++ test/game_test.py
@@ -1,4 +1,21 @@
 
-# TODO: import some code
+# import some code
+from app.game import determine_winner
 
-# TODO: test the code
+# test the code
+def test_determine_winner():
+
+    # If user input is rock
+    assert determine_winner("rock" , "rock") == "None"
+    assert determine_winner("rock", "paper") == "Computer"
+    assert determine_winner("rock", "scissors") == "User"
+
+    # If user input is paper
+    assert determine_winner("paper" ,"rock") == "User"
+    assert determine_winner("paper", "paper") == "None"
+    assert determine_winner("paper", "scissors") == "Computer"
+
+    # If user input is scissors
+    assert determine_winner("scissors" ,"rock") == "Computer"
+    assert determine_winner("scissors", "paper") == "User"
+    assert determine_winner("scissors", "scissors") == "None"
